d39815dbdbfca5375b97e5f8a5018bca59de87d2
Rakib <phpzarakib@gmail.com>
2022-04-30 10:02:46 +0600

    start

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 function App() {
   return (
     <div >
-      <h1>Admin</h1>
+      <h1>Admin are high-emphasis, distinguished by their use of elevation and fill. They contain actions that are primary to your app.</h1>
     </div>
   );
 }
